4f08542e9565f9f618b0cca6d8ec5ee31b76b041
Anas Khan <muhammadanaskhanak@gmail.com>
2025-07-18 03:54:54 +0500

    Create Credit.jsx

---
+++ src/components/Credit.jsx
@@ -0,0 +1,18 @@
+export default function Credit(){
+    return (
+        <>
+           <div className="absolute right-0 bottom-0 text-[13px]  text-end w-fit text-white leading-relaxed tracking-wide hidden md:block lg:block">
+          <p>
+            Design & Developed By
+            <a
+              href="https://www.linkedin.com/in/muhammad-anas-khan786"
+              className="ml-1 underline"
+              target="_blank"
+            >
+              Anas Khan
+            </a>
+          </p>
+        </div>
+        </>
+    );
+}
