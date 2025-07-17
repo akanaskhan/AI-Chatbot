17833a1227db5282eff93da83218290fd2203054
Anas Khan <muhammadanaskhanak@gmail.com>
2025-07-18 02:34:01 +0500

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,14 +8,14 @@ export default function Home() {
             <div className="relative">
 
                 <ChatWithAI />
-                {/* <div className="absolute bottom-0 -z-50">
+                <div className="absolute bottom-0 -z-50">
                     <div className="ball-container">
   <div className="blurred-ball ball1"></div>
   <div className="blurred-ball ball2"></div>
   <div className="blurred-ball ball3"></div>
 </div>
 
-                </div> */}
+                </div>
             </div>
         </>
     );
